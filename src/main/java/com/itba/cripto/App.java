bda019331c59e14c884f8683848c3473bac323c3
jcgrethe <ext_jgrethe@mercadolibre.com>
2020-04-13 17:06:52 -0300

    java project

---
+++ src/main/java/com/itba/cripto/App.java
@@ -0,0 +1,13 @@
+package com.itba.cripto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

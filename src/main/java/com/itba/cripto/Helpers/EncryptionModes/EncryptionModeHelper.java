bfc0608701d2d44ba5bcbf479e71c7153fbb21ed
fermartin17 <femartin@itba.edu.ar>
2020-05-15 16:55:32 -0300

    Encryption modes working

--- src/main/java/com/itba/cripto/Helpers/EncryptionModes/EncryptionModeHelper.java
+++ src/main/java/com/itba/cripto/Helpers/EncryptionModes/EncryptionModeHelper.java
@@ -1,11 +1,13 @@
 package com.itba.cripto.Helpers.EncryptionModes;
 
 import com.itba.cripto.Helpers.Constant.Constants;
+import com.itba.cripto.Helpers.Factories.IVLenghtFactory;
 import com.itba.cripto.Helpers.Factories.SchemeFactory;
 import com.itba.cripto.Interfaces.EncriptionMode;
 import lombok.Builder;
 
 import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
@@ -13,11 +15,14 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Base64;
 
+import static com.itba.cripto.Helpers.Constant.Constants.ConstantsValues.ECB;
+
 public class EncryptionModeHelper implements EncriptionMode {
 
 
     private byte[] key;
     private SecretKeySpec secretKey;
+    private IvParameterSpec iv;
     private final String mode;
 
     public EncryptionModeHelper(String mode){
@@ -29,10 +34,13 @@ public class EncryptionModeHelper implements EncriptionMode {
         MessageDigest sha = null;
         try {
             int keyLenght = SchemeFactory.GetScheme(scheme);
+            int ivLenght = IVLenghtFactory.GetIVLenght(scheme);
             key = myKey.getBytes("UTF-8");
             sha = MessageDigest.getInstance("SHA-256");
             key = sha.digest(key);
             key = Arrays.copyOf(key, keyLenght);
+            int requiredLength = keyLenght + ivLenght;
+            this.iv = new IvParameterSpec(Arrays.copyOfRange(key, keyLenght, requiredLength));
             if(keyLenght == 8)
                 secretKey = new SecretKeySpec(key, "DES");
             else
@@ -58,7 +66,10 @@ public class EncryptionModeHelper implements EncriptionMode {
                 mode  = String.format("AES/%s/PKCS5PADDING",this.mode);
             setKey(secret,scheme);
             Cipher cipher = Cipher.getInstance(mode);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+            if(this.mode.toLowerCase().compareTo(ECB) != 0)
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey,iv);
+            else
+                cipher.init(Cipher.ENCRYPT_MODE, secretKey);
             return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
         }
         catch (Exception e)
@@ -80,7 +91,10 @@ public class EncryptionModeHelper implements EncriptionMode {
                 mode  = String.format("AES/%s/PKCS5PADDING",this.mode);
             setKey(secret,scheme);
             Cipher cipher = Cipher.getInstance(mode);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
+            if(this.mode.toLowerCase().compareTo(ECB) != 0)
+                cipher.init(Cipher.DECRYPT_MODE, secretKey,iv);
+            else
+                cipher.init(Cipher.DECRYPT_MODE, secretKey);
             return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
         }
         catch (Exception e)

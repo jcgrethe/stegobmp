48774929c179233a0b47d600ac851de2c41ebefb
fermartin17 <femartin@itba.edu.ar>
2020-06-05 17:29:17 -0300

    RC4

--- src/test/java/com/itba/cripto/test.java
+++ src/test/java/com/itba/cripto/test.java
@@ -1,21 +1,25 @@
 package com.itba.cripto;
 
 import com.itba.cripto.Helpers.EncryptionModes.EncryptionModeHelper;
+import com.itba.cripto.Helpers.EncryptionModes.RC4ModeHelper;
 import org.apache.commons.lang.RandomStringUtils;
 import org.junit.jupiter.api.Test;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.util.concurrent.ThreadLocalRandom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class test {
 
-
-
-
     @Test
-    public void encrypt() {
+    public void encrypt() throws NoSuchPaddingException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         test("cbc","aes128");
         test("cbc","aes192");
         test("cbc","aes256");
@@ -35,6 +39,9 @@ public class test {
         test("ofb","aes192");
         test("ofb","aes256");
         test("ofb","des");
+
+        testRC4();
+
     }
 
 
@@ -48,6 +55,18 @@ public class test {
         EncryptionModeHelper decryptionModeHelper = new EncryptionModeHelper(mode);
 
         byte[] result = decryptionModeHelper.decrypt(data, pass, scheme);
+        assertEquals(secret, new String(result));
+    }
+
+    private void testRC4() throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException {
+        RC4ModeHelper rc4 = new RC4ModeHelper();
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        String secret = RandomStringUtils.randomAlphanumeric(random.nextInt(100, 20000));
+        String pass = RandomStringUtils.randomAlphanumeric(random.nextInt(5, 16));
+        byte[] data = rc4.encrypt(secret.getBytes(), pass.getBytes());
+
+
+        byte[] result = rc4.decrypt(data, pass.getBytes());
         assertEquals(secret, new String(result));
     }
 
